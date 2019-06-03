fae712c97c1f49a8a08b5e7d76ea375df6815fa3
Сергей Русинов <fftron@me.com>
2019-06-03 22:01:47 +0500

    web

---
+++ TattooParlor.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TattooParlor.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
